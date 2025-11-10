897e4dff08269d87de641ff7b453c8fb72dffa64
ocb-dev-droid <oliver.coughlanbryan.290@accesscreative.ac.uk>
2025-11-10 12:22:47 +0000

    V1.2 Progression framework
    XP framework implemaneted, Unity image bug preventing me from effective testing, working on fix.

---
+++ ADemo/Assets/Scripts/Mechanics/Progression.cs
@@ -0,0 +1,63 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class Progression : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] TextMeshProUGUI currentLevelText;
+    [SerializeField] TextMeshProUGUI xpText;
+    [SerializeField] UnityEngine.UI.Image xpBar;
+
+    
+    
+    public int targetXP = 100;
+    [SerializeField] int targetXPIncrease = 100;
+
+    int currentLevel;
+    int currentXP;
+
+    public void IncreaseXP(int amount)
+    {
+        currentXP += amount;
+        CheckForLevelUp();
+        updateUI();
+    }
+    
+    private void CheckForLevelUp()
+    {
+        while(currentXP >= targetXP)
+        {
+            currentLevel++;
+            currentXP -= targetXP;
+            targetXP += targetXPIncrease;
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentLevel = 1;
+        updateUI();
+    }
+
+    private void updateUI()
+    {
+        currentLevelText.text = "Level " + currentLevel;
+        xpText.text = currentXP + "/" + targetXP;
+        xpBar.fillAmount = (float)currentXP / (float)targetXP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  
+
+
+}
