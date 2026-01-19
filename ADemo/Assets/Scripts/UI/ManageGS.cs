36119cf6bfc854504e78c6e82caf6c28a776e32e
ocb-dev-droid <oliver.coughlanbryan.290@accesscreative.ac.uk>
2026-01-19 14:59:51 +0000

    Pause Update (broken)

--- ADemo/Assets/Scripts/UI/ManageGS.cs
+++ ADemo/Assets/Scripts/UI/ManageGS.cs
@@ -1,29 +1,37 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PauseMenu : MonoBehaviour
+public class ManageGS : MonoBehaviour
 {
+   bool isPaused = false;
     public GameObject PauseUI;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-        PauseUI.SetActive(false);
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+       UnPauseGame();
     }
 
     // Update is called once per frame
     void Update()
     {
        if(Input.GetKeyDown(KeyCode.Escape));
-        {
-            PauseGame();
+        {if (isPaused)
+                UnPauseGame();
+         else PauseGame();
         }
     }
 
+    public void UnPauseGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1;
+        PauseUI.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
     public void PauseGame()
     {
+        isPaused = true;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         Time.timeScale = 0;
--- ADemo/Assets/Scripts/UI/MenuUI/MainMenu.cs
+++ ADemo/Assets/Scripts/UI/MenuUI/MainMenu.cs
@@ -1,7 +1,10 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    int resWidth = 1920;
+    int resLen = 1080;
     public void StartButton()
     {
         SceneManager.LoadScene("HubWorld");
